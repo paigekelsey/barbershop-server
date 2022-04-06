a490888adbf7998ff8078f8f710d3945eaae8fd8
Kelsey Smith <pages.kelsey@gmail.com>
2022-04-06 18:31:17 -0400

    hooked up barbers database/prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BarbersController } from './barbers/barbers.controller';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BarbersController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
---
+++ src/barbers/barbers.controller.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Controller('barbers')
+export class BarbersController {
+  @Get()
+  async findAll() {
+    const client = new PrismaClient();
+    const barbers = await client.barber.findMany();
+    return barbers;
+  }
+}
