a824c62f42008b8dd64842fd533a2e8bf0b96ddb
Kelsey Smith <pages.kelsey@gmail.com>
2022-04-06 19:41:49 -0400

    added get/post routes -working

--- src/barbers/barbers.controller.ts
+++ src/barbers/barbers.controller.ts
@@ -1,13 +1,90 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+
+import { Barber } from '@prisma/client';
+
+import { PrismaService } from 'src/prisma/prisma.service';
+
+export interface BarberDto {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export function barberToDto(barber: Barber) {
+  const dto: BarberDto = {
+    id: barber.id,
+    name: barber.name,
+    description: barber.description,
+  };
+  return dto;
+}
+
+interface CreateBarberDto {
+  name: string;
+  description: string;
+}
+
+interface UpdateBarberDto {
+  id: number;
+  name?: string;
+  description?: string;
+}
 
 @Controller('barbers')
 export class BarbersController {
+  constructor(private prisma: PrismaService) {}
+
   @Get()
   async findAll() {
-    const client = new PrismaClient();
-    const barbers = await client.barber.findMany();
-    return barbers;
+    const barbers = await this.prisma.barber.findMany();
+    const barbersDto = barbers.map(barberToDto);
+    return barbersDto;
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) barberId: number) {
+    const barber = await this.prisma.barber.findFirst({
+      where: { id: barberId },
+    });
+    if (barber == null) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+    const barberDto = barberToDto(barber);
+    return barberDto;
+  }
+
+  @Post()
+  async createBarber(@Body() dto: CreateBarberDto) {
+    const newBarber = await this.prisma.barber.create({
+      data: {
+        name: dto.name,
+        description: dto.description,
+      },
+    });
+    const newBarberDto = barberToDto(newBarber);
+    return newBarberDto;
+  }
+
+  @Post('/update')
+  async updateBarber(@Body() dto: UpdateBarberDto) {
+    const barber = await this.prisma.barber.update({
+      where: { id: dto.id },
+      data: {
+        name: dto.name,
+        description: dto.description,
+      },
+    });
+    const barberDto = barberToDto(barber);
+    return barberDto;
   }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
